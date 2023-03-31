2a7a9ee3a63b8726851486f9c2fa4c38ceeec0cd
Egor Lynko <flexoid@gmail.com>
2023-03-31 20:43:22 +0200

    Allow to set config path via env variable

--- main.go
+++ main.go
@@ -29,7 +29,12 @@ type MergeRequestWithApprovals struct {
 }
 
 func main() {
-	config, err := readConfig("config.yaml")
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configPath = "config.yaml"
+	}
+
+	config, err := readConfig(configPath)
 	if err != nil {
 		fmt.Printf("Error reading configuration file: %v\n", err)
 		os.Exit(1)
